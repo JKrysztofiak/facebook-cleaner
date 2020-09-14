030af3c1357ba1f0741b5b8d83a47aef971ae5e3
JKrysztofiak <jakub.krysztofiak@protonmail.com>
2020-09-14 12:29:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+function removeDistractions() {
+	const feed = document.querySelector('[role=feed]');
+	const stories = document.querySelector('[data-pagelet=Stories]');
+	const rooms = document.querySelector('[data-pagelet=VideoChatHomeUnit]');
+	const watch = document.querySelector('[aria-label=Watch]');
+
+	if (feed == null) {
+		return;
+	}
+
+	const msg = document.createElement('h1');
+	msg.innerText = 'News Feed has been REMOVED';
+	msg.style.color = 'white';
+
+	feed.style.visibility = 'hidden';
+	stories.style.display = 'none';
+	rooms.style.display = 'none';
+	watch.parentElement.parentElement.parentElement.style.display = 'none';
+
+	setTimeout(() => {
+		feed.parentElement.insertBefore(msg, feed);
+	}, 1000);
+
+	console.log('FEED REMOVED');
+}
+
+setInterval(removeDistractions(), 1000);
